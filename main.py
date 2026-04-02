b2986ae584e8857d1175c6ba8cde3d4491001679
Alicia Betancourt <rococo@Alicias-MacBook-Air.local>
2026-04-02 21:23:22 +0200

    Doc(main.py): extract comments and create README and other documentation

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
+import con
 from machine import ADC, Pin #machine to access the hardware
 import time
-
-# ------------------------
-# CONTEXT
-# ------------------------
-
-#read digital (0/1)
-#write HIGH/LOW
-#set-up pull-up/down
-
-#ADC reads microphone voltage
-#ADC range: 0 → 65535
-#ADC with 12 bits
-#ADC converts pin into analog sign I/
-# read_u16() read voltag and return nb
-#silent → 3000–8000
-#normal noise → 10000–25000
-#stornger noise → 30000–60000
 
 # ------------------------
 # CONFIG
@@ -56,9 +40,9 @@ while True:
 
     # Basic filter
     value = (value + baseline) / 2
-    print(f{"value after filter: "; value)
+    print(f"value after filter: {value}")
     # Alert
     if value > THRESHOLD:
         print("WARNING: too loud!")
     
-    time.sleep(0.1)
+    time.sleep(SAMPLE_TIME)
